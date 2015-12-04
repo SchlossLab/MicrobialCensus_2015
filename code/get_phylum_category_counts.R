027813e8915d0a0fd07ba9d749c76b44ac9cc976
Pat Schloss <pschloss@umich.edu>
2015-12-04 13:31:15 -0500

    Output counts instead of relative abundances

--- code/get_phylum_category_counts.R
+++ code/get_phylum_category_counts.R
@@ -15,10 +15,10 @@ bact_phylum[bact_phylum=="Euryarchaeota"] <- NA #to be removed
 bact_phylum[bact_phylum=="aquifer1"] <- NA
 
 b_phylum_cat_count <- table(bact_phylum, bact$category)
-b_phylum_cat_ra <- prop.table(b_phylum_cat_count, margin=2)
-b_phylum_cat_ra <- cbind(b_phylum_cat_ra, total=table(bact_phylum)/length(bact_phylum))
-rownames(b_phylum_cat_ra) <- gsub("_?\\(.*\\)", "", rownames(b_phylum_cat_ra) )
-bacterial_data <- cbind(domain=rep("bacteria", nrow(b_phylum_cat_ra)), b_phylum_cat_ra)
+#b_phylum_cat_ra <- prop.table(b_phylum_cat_count, margin=2)
+#b_phylum_cat_ra <- cbind(b_phylum_cat_ra, total=table(bact_phylum)/length(bact_phylum))
+rownames(b_phylum_cat_count) <- gsub("_?\\(.*\\)", "", rownames(b_phylum_cat_count) )
+bacterial_data <- cbind(domain=rep("bacteria", nrow(b_phylum_cat_count)), b_phylum_cat_count)
 
 
 arch <- read.table(file='data/process/archaea.v123.metadata', header=T, row.names=1, stringsAsFactors=FALSE)
@@ -27,10 +27,10 @@ arch$category[arch$category=="BDBD" & !is.na(arch$category)] <- "BD" #to remove
 arch_phylum <- get_phylum(arch$taxonomy)
 
 a_phylum_cat_count <- table(arch_phylum, factor(arch$category, levels=names(categories)))
-a_phylum_cat_ra <- prop.table(a_phylum_cat_count, margin=2)
-a_phylum_cat_ra <- cbind(a_phylum_cat_ra, total=table(arch_phylum)/length(arch_phylum))
-rownames(a_phylum_cat_ra) <- gsub("_?\\(.*\\)", "", rownames(a_phylum_cat_ra) )
-archaeal_data <- cbind(domain=rep("archaea", nrow(a_phylum_cat_ra)), a_phylum_cat_ra)
+#a_phylum_cat_ra <- prop.table(a_phylum_cat_count, margin=2)
+#a_phylum_cat_ra <- cbind(a_phylum_cat_ra, total=table(arch_phylum)/length(arch_phylum))
+rownames(a_phylum_cat_count) <- gsub("_?\\(.*\\)", "", rownames(a_phylum_cat_count) )
+archaeal_data <- cbind(domain=rep("archaea", nrow(a_phylum_cat_count)), a_phylum_cat_count)
 
 
 write.table(rbind(bacterial_data, archaeal_data), file="data/process/phylum_category_counts.tsv", sep='\t', row.names=T, quote=F)
