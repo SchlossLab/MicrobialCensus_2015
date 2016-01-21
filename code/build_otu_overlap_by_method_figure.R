a6880629ae10b66ab960794df5b92d1a235e2f07
Pat Schloss <pschloss@umich.edu>
2016-01-21 15:39:56 -0500

    Add legend, title, and color

--- code/build_otu_overlap_by_method_figure.R
+++ code/build_otu_overlap_by_method_figure.R
@@ -1,7 +1,7 @@
+#devtools::install_github("karthik/wesanderson")
+library("wesanderson")
 
-overlap <- read.table(file="data/process/otu_overlap_by_method.tsv", header=T)
-
-
+palette <- "Darjeeling2"
 
 plot_overlap <- function(domain){
 
@@ -13,13 +13,14 @@ plot_overlap <- function(domain){
 
 	d_subset <- d_overlap[c("sc", "em_pcr", "em_meta"), c("cultured", "pcr", "sc", "em_pcr", "em_meta")]
 
-	barplot(t(as.matrix(d_subset)), beside=T, ylim=c(0,105), axes=F, ylab="", names.arg=c("","",""))
+	barplot(t(as.matrix(d_subset)), beside=T, ylim=c(0,105), axes=F, ylab="", names.arg=c("","",""), col=wes_palette(palette))
 	axis(2, las=2)
 	box()
-
+	text(x=17, y=100, gsub("^(.)", "\\U\\1", domain, perl=TRUE), cex=1.5, font=2)
 }
 
 
+overlap <- read.table(file="data/process/otu_overlap_by_method.tsv", header=T)
 
 
 pdf(file="results/figures/otu_overlap_by_method.pdf", width=6.0, height=5.0)
@@ -27,6 +28,8 @@ pdf(file="results/figures/otu_overlap_by_method.pdf", width=6.0, height=5.0)
 layout(matrix(c(3,1,3,2,0,4), nrow=3, byrow=T), widths=c(0.1,1), heights=c(1, 1, 0.18))
 plot_overlap("bacteria")
 plot_overlap("archaea")
+
+legend(x=5, y=80, legend=c("Cultured", "PCR", "Single Cell", "EMIRGE (PCR)", "EMIRGE (Metagenome)"), fill=wes_palette(palette))
 
 plot.new()
 par(mar=c(0,0,0,0))
@@ -39,8 +42,3 @@ text(x=10.0,y=0.6, "EMIRGE\n(PCR)", cex=1.4, xpd=T)
 text(x=16.3,y=0.6, "EMIRGE\n(Metagenome)", cex=1.4, xpd=T)
 
 dev.off()
-
-
-# Domain headings
-# Legend
-# Color scheme
